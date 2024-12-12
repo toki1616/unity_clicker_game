832761af08a8a2ffbca415d0bb89fa715d1da76e
toki1616 <tokigit@gmail.com>
2024-12-12 21:38:02 +0900

    ScreenExの処理に改行の追加

--- clicker_game/Assets/Clicker/Components/Screen/Extension/ScreenEx.cs
+++ clicker_game/Assets/Clicker/Components/Screen/Extension/ScreenEx.cs
@@ -13,18 +13,25 @@ namespace My.ClickerGame.Ex
             {
                 case ScreenType.Home:
                     return AddressableConst.homeUI;
+
                 case ScreenType.BattleSelect:
                     return AddressableConst.battleSelectUI;
+
                 case ScreenType.Battle:
                     return AddressableConst.battleUI;
+
                 case ScreenType.GachaSelect:
                     return AddressableConst.gachaSelectUI;
+
                 case ScreenType.Gacha:
                     return AddressableConst.gachaUI;
+
                 case ScreenType.Other:
                     return AddressableConst.otherUI;
+
                 case ScreenType.Setting:
                     return AddressableConst.settingUI;
+
                 default:
                     return AddressableConst.otherUI;
             }
@@ -36,18 +43,25 @@ namespace My.ClickerGame.Ex
             {
                 case ScreenType.Home:
                     return ScreenSize.SafeArea;
+
                 case ScreenType.BattleSelect:
                     return ScreenSize.SafeArea;
+
                 case ScreenType.Battle:
                     return ScreenSize.SafeArea;
+
                 case ScreenType.GachaSelect:
                     return ScreenSize.SafeArea;
+
                 case ScreenType.Gacha:
                     return ScreenSize.SafeArea;
+
                 case ScreenType.Other:
                     return ScreenSize.SafeArea;
+
                 case ScreenType.Setting:
                     return ScreenSize.SafeArea;
+
                 default:
                     return ScreenSize.SafeArea;
             }
